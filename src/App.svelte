9d35bbcf6fe1e61987c24199a2370f5a2de58329
jmaen <jammi1104@gmail.com>
2022-11-23 10:10:29 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,186 @@
+<script>
+  import projects from './assets/projects.json'
+  import Project from './lib/Project.svelte'
+</script>
+
+<main>
+  <div>
+    <div>
+      <h1>Hello, welcome to my website!</h1>
+      <h2>Here you can find a collection of some of my personal projects.</h2>
+    </div>
+    <div class="projects">
+      <div class="projects-grid">
+        {#each projects as project}
+          <Project name={project.name} description={project.description} theme={project.theme} link={project.link}/>
+        {/each}
+      </div>
+    </div>
+  </div>
+  <div class="socials">
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+      <a href="https://github.com/jmaen">/jmaen</a>
+    </div>
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
+      <a href="https://twitter.com/jannikmaenzer">/jannikmaenzer</a>
+    </div>
+  </div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  /* Headline */
+
+  h1 {
+    position: relative;
+    margin: 0 2vw;
+    margin-bottom: 1.25rem;
+    color: var(--primary);
+    font-family: "JetBrains Mono", monospace;
+    font-size: 4rem;
+    text-align: center;
+  }
+
+  @keyframes typing {
+    to {
+      left: 100%;
+    }
+  }
+
+  h1::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: var(--background);
+    animation: typing 1.5s steps(29) 0.5s forwards;
+  }
+
+  @keyframes fade-in {
+    to {
+      opacity: 1;
+    }
+  }
+
+  h2 {
+    margin: 0 2vw;
+    color: var(--secondary);
+    font-size: 1.75rem;
+    font-weight: 500;
+    text-align: center;
+    opacity: 0;
+    animation: fade-in 2s 1.5s forwards;
+  }
+
+  @media screen and (max-width: 1200px) {
+    h1 {
+      font-size: 3.5rem;
+    }
+
+    h2 {
+      font-size: 1.5rem;
+    }
+  }
+
+  @media screen and (max-width: 900px) {
+    h1 {
+      font-size: 3rem;
+    }
+
+    h2 {
+      font-size: 1.25rem;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    h1 {
+      opacity: 0;
+      animation: fade-in 2s forwards;
+    }
+
+    h1::before {
+      display: none;
+    }
+
+    h2 {
+      animation: fade-in 2s forwards;
+    }
+  }
+
+  /* Projects */
+
+  .projects {
+    display: flex;
+    justify-content: center;
+    margin-top: 10vh;
+    opacity: 0;
+    animation: fade-in 2s 2s forwards;
+  }
+
+  .projects-grid {
+    display: inline-grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 2rem;
+  }
+
+  @media screen and (max-width: 1024px) {
+    .projects-grid {
+      grid-template-columns: 1fr 1fr;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .projects {
+      margin-top: 5vh;
+      animation: fade-in 2s 0.5s forwards;
+    }
+
+    .projects-grid {
+      grid-template-columns: 1fr;
+    }
+  }
+
+  /* Socials */
+
+  .socials {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+    margin: 1rem;
+  }
+
+  .socials div {
+    display: flex;
+  }
+
+  .socials div + div {
+    margin-top: 1rem;
+  }
+
+  .socials svg {
+    height: 1.5rem;
+    margin-right: 0.5rem;
+    fill: var(--primary);
+  }
+
+  .socials a {
+    color: var(--primary);
+    font-weight: 500;
+    text-decoration: none;
+  }
+
+  .socials a:hover {
+    text-decoration: underline;
+  }
+</style>
---
+++ src/lib/Project.svelte
@@ -0,0 +1,80 @@
+<script>
+    export let name
+    export let description
+    export let theme
+    export let link
+
+    function test() {
+        if(link) {
+            window.open(link, "_blank")
+        } else {
+            window.open(name, "_self")
+        }
+    }
+</script>
+
+<button class="project" on:click={test}>
+    <div class="project-thumbnail" style="background-color: {theme};">
+        <img src="/icons/{name}.svg" alt="">
+    </div>
+    <div class="project-info">
+        <p class="project-title">/{name}</p>
+        <p class="project-description">{description}</p>
+    </div>
+</button>
+
+<style>
+    .project {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 22rem;
+        height: 6rem;
+        padding: 0.5rem;
+        border: 1px solid var(--gray);
+        border-radius: 5px;
+        background-color: transparent;
+        text-align: left;
+        cursor: pointer;
+    }
+
+    .project-thumbnail {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 4rem;
+        width: 4rem;
+        border-radius: 5px;
+    }
+
+    .project-thumbnail img {
+        width: 2rem;
+        height: 2rem;
+        filter: brightness(0) invert(1);
+        transition: transform 0.3s ease-in-out;
+    }
+
+    .project:hover .project-thumbnail img {
+        transform: scale(1.25);
+    }
+
+    .project-info {
+        margin-left: 1rem;
+    }
+
+    .project-title {
+        color: var(--primary);
+        font-weight: 600;
+        text-decoration: none;
+    }
+
+    .project:hover .project-title {
+        text-decoration: underline;
+    }
+
+    .project-description {
+        width: 16rem;
+        color: var(--secondary);
+        font-size: 0.9rem;
+    }
+</style>
